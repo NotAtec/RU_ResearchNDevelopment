74f9b2010a22df1266f62a750d9cc76a3303f6bb
Jeroen Brinkhorst <jeroenb04@live.com>
2023-05-17 12:38:12 +0200

    feat: Backend workings for a friends-system

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, presence:true, uniqueness: true
+  validates :username, presence:true, uniqueness: true
+  
+
+  has_many :requests, class_name: 'FriendRequest', foreign_key: 'requester_id', dependent: :destroy
+  has_many :received_requests, class_name: 'FriendRequest', foreign_key: 'requestee_id', dependent: :destroy
+
+  def friends
+    requests.where(confirmed: true).map(&:requestee) + received_requests.where(confirmed: true).map(&:requester)
+  end
+
+  def incoming_requests
+    received_requests.where(confirmed: false)
+  end
+
+  def outgoing_requests
+    requests.where(confirmed: false)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_124458) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_17_102420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.bigint "requester_id", null: false
+    t.bigint "requestee_id", null: false
+    t.boolean "confirmed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["requestee_id"], name: "index_friend_requests_on_requestee_id"
+    t.index ["requester_id"], name: "index_friend_requests_on_requester_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,8 +37,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_124458) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friend_requests", "users", column: "requestee_id"
+  add_foreign_key "friend_requests", "users", column: "requester_id"
 end
